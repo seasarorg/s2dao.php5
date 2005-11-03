742b0d25b5011a497c2d63f1c463a8a89e5564cd
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2005-11-03 18:07:56 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@37 18cb923b-d800-0410-ad6b-a912cf186b9e

---
+++ s2dao.inc.php
@@ -0,0 +1,66 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+// +----------------------------------------------------------------------+
+// | PHP version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003-2004 The Seasar Project.                          |
+// +----------------------------------------------------------------------+
+// | The Seasar Software License, Version 1.1                             |
+// |   This product includes software developed by the Seasar Project.    |
+// |   (http://www.seasar.org/)                                           |
+// +----------------------------------------------------------------------+
+// | Authors: nowel                                                       |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+/**
+ * @author nowel
+ */
+
+/** -------------- S2Container.PHP5 Setting -----------------------------*/
+
+/** S2Container.PHP5 ROOT Directory */
+define('S2CONTAINER_PHP5', dirname(__FILE__) . "/src/s2container.php5");
+/** S2Dao.PHP5 ROOT Ditrctory */
+define("S2DAO_PHP5", dirname(__FILE__) . "/src/s2dao.php5");
+
+/** DICON XML format DTD Validation Switch */
+define("S2CONTAINER_PHP5_DOM_VALIDATE", false);
+
+/**
+ * Messages Resouce File
+ * S2Container.php5 と S2Dao.php5 のメッセージをマージしたものです。
+ */
+define("S2CONTAINER_PHP5_MESSAGES_INI", S2DAO_PHP5 . "/SSRMessages.properties");
+
+/** S2Container.PHP5 Log Level */
+//define('S2CONTAINER_PHP5_LOG_LEVEL',SimpleLogger::WARN);
+
+/** SingletonS2ContainerFactory app.dicon */
+//define('S2CONTAINER_PHP5_APP_DICON','app.dicon');
+
+require_once(S2CONTAINER_PHP5 . "/S2ContainerClassLoader.class.php");
+require_once(S2CONTAINER_PHP5 . "/s2container.core.classes.php");
+require_once(S2DAO_PHP5 . "/S2DaoClassLoader.class.php");
+
+/** if use PEAR::DB */
+require_once("DB.php");
+
+if( function_exists("__autoload") ){
+    exit("sorry. is already __autoload()");
+}
+
+/** __autoload function */
+if( class_exists("S2ContainerClassLoader") && class_exists("S2DaoClassLoader") ){
+    
+    function __autoload($class = null){
+        if( S2ContainerClassLoader::load($class) ){
+            return;
+        }
+        if( S2DaoClassLoader::load($class) ){
+            return;
+        }
+    }
+}
+
+?>
