ef42131b999a8bcd8bc68ca47e60d11fbb84e5fc
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2005-11-07 17:52:18 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@41 18cb923b-d800-0410-ad6b-a912cf186b9e

--- src/examples/CdDao.class.php
+++ src/examples/CdDao.class.php
@@ -8,9 +8,9 @@ interface CdDao {
     const List_getCD1_ARGS = "id, title";
     const List_getCD2_ARGS = "id, title, content";
     
-    public function update($cd);
-    public function insert($cd);
-    public function delete($cd);
+    public function update(CdBean $cd);
+    public function insert(CdBean $cd);
+    public function delete(CdBean $cd);
     
     public function Array_getAllCd();
     public function List_getCD1($id, $title = null);
