4abfe9efdfdf5dbfef8a80769dbc9b5743fa2fd5
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2005-11-18 04:39:04 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@56 18cb923b-d800-0410-ad6b-a912cf186b9e

--- s2dao.inc.php
+++ s2dao.inc.php
@@ -17,64 +17,26 @@
  * @author nowel
  */
 
-/** -------------- S2Container.PHP5 Setting -----------------------------*/
-
-/** S2Container.PHP5 ROOT Directory */
-define('S2CONTAINER_PHP5', dirname(__FILE__) . "/src/s2container.php5");
-/** S2Dao.PHP5 ROOT Ditrctory */
+/** S2Dao.PHP5 ROOT Directory */
 define("S2DAO_PHP5", dirname(__FILE__) . "/src/s2dao.php5");
-
-/** DICON XML format DTD Validation Switch */
-define("S2CONTAINER_PHP5_DOM_VALIDATE", false);
 
 /**
  * Messages Resouce File
- * S2Container.php5 と S2Dao.php5 のメッセージをマージしたものです。
  */
-define("S2CONTAINER_PHP5_MESSAGES_INI", S2DAO_PHP5 . "/SSRMessages.properties");
+define("S2DAO_PHP5_MESSAGES_INI", S2DAO_PHP5 . "/DaoMessages.properties");
 
-/** S2Container.PHP5 Log Level */
-//define('S2CONTAINER_PHP5_LOG_LEVEL',SimpleLogger::WARN);
-
-/** SingletonS2ContainerFactory app.dicon */
-//define('S2CONTAINER_PHP5_APP_DICON','app.dicon');
-
-require_once(S2CONTAINER_PHP5 . "/S2ContainerClassLoader.class.php");
-require_once(S2CONTAINER_PHP5 . "/s2container.core.classes.php");
 require_once(S2DAO_PHP5 . "/S2DaoClassLoader.class.php");
 
-/** if use PEAR::DB */
+if( class_exists("S2Container_MessageUtil") ){
+    S2Container_MessageUtil::addMessageResource(S2DAO_PHP5_MESSAGES_INI);
+}
+
+if( class_exists("S2ContainerClassLoader") ){
+    S2ContainerClassLoader::$USER_CLASSES = array_merge(
+            S2ContainerClassLoader::$USER_CLASSES, S2DaoClassLoader::export());
+}
+
+/** use PEAR::DB */
 require_once("DB.php");
-
-if( function_exists("__autoload") ){
-    exit("sorry. is already __autoload()");
-}
-
-// when beta2
-function unsetS2ContainerClsLoderKey($key){
-    if(isset(S2ContainerClassLoader::$CLASSES[$key])){
-        unset(S2ContainerClassLoader::$CLASSES[$key]);
-    }
-}
-
-/** __autoload function */
-if( class_exists("S2ContainerClassLoader") && class_exists("S2DaoClassLoader") ){
-
-    // unset
-    unsetS2ContainerClsLoderKey("DaoMetaData");
-    unsetS2ContainerClsLoderKey("DaoMetaDataImpl");
-    unsetS2ContainerClsLoderKey("DaoMetaDataFactory");
-    unsetS2ContainerClsLoderKey("DaoMetaDataFactoryImpl");
-    unsetS2ContainerClsLoderKey("SqlCommand");
-    
-    function __autoload($class = null){
-        if( S2ContainerClassLoader::load($class) ){
-            return;
-        }
-        if( S2DaoClassLoader::load($class) ){
-            return;
-        }
-    }
-}
 
 ?>
