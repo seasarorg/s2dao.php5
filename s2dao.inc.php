52f67f7977cf6a14c39f67edd2214d7e329f57bf
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2005-11-19 19:55:33 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@58 18cb923b-d800-0410-ad6b-a912cf186b9e

--- s2dao.inc.php
+++ s2dao.inc.php
@@ -3,15 +3,22 @@
 // +----------------------------------------------------------------------+
 // | PHP version 5                                                        |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2003-2004 The Seasar Project.                          |
+// | Copyright 2004-2005 the Seasar Foundation and the Others.            |
 // +----------------------------------------------------------------------+
-// | The Seasar Software License, Version 1.1                             |
-// |   This product includes software developed by the Seasar Project.    |
-// |   (http://www.seasar.org/)                                           |
+// | Licensed under the Apache License, Version 2.0 (the "License");      |
+// | you may not use this file except in compliance with the License.     |
+// | You may obtain a copy of the License at                              |
+// |                                                                      |
+// |     http://www.apache.org/licenses/LICENSE-2.0                       |
+// |                                                                      |
+// | Unless required by applicable law or agreed to in writing, software  |
+// | distributed under the License is distributed on an "AS IS" BASIS,    |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                        |
+// | either express or implied. See the License for the specific language |
+// | governing permissions and limitations under the License.             |
 // +----------------------------------------------------------------------+
-// | Authors: nowel                                                       |
+// | Authors: klove                                                       |
 // +----------------------------------------------------------------------+
-//
 // $Id$
 /**
  * @author nowel
@@ -27,13 +34,11 @@ define("S2DAO_PHP5_MESSAGES_INI", S2DAO_PHP5 . "/DaoMessages.properties");
 
 require_once(S2DAO_PHP5 . "/S2DaoClassLoader.class.php");
 
+if( class_exists("S2ContainerClassLoader") ){
+    S2ContainerClassLoader::import(S2DaoClassLoader::export());
+}
 if( class_exists("S2Container_MessageUtil") ){
     S2Container_MessageUtil::addMessageResource(S2DAO_PHP5_MESSAGES_INI);
-}
-
-if( class_exists("S2ContainerClassLoader") ){
-    S2ContainerClassLoader::$USER_CLASSES = array_merge(
-            S2ContainerClassLoader::$USER_CLASSES, S2DaoClassLoader::export());
 }
 
 /** use PEAR::DB */
