0dbe6e0a368d5067f3e2466b5578a0b4d609b608
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2005-12-27 02:41:54 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@169 18cb923b-d800-0410-ad6b-a912cf186b9e

--- src/examples/Cd2DaoClient.class.php
+++ src/examples/Cd2DaoClient.class.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__) . "/example.inc.php";
 
-$container = S2ContainerFactory::create("example.dicon.xml");
+$container = S2ContainerFactory::create("./resource/example.dicon.xml");
 $dao = $container->getComponent("daoImpl");
 
 var_dump($dao->getCd(2));
