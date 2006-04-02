a405ff6d8eb60bfaa2dea9a70e2dfbd6ea87985a
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2006-04-02 14:27:42 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@240 18cb923b-d800-0410-ad6b-a912cf186b9e

--- examples/example.inc.php
+++ examples/example.inc.php
@@ -1,15 +1,14 @@
 <?php
-require_once dirname(dirname(__FILE__)) . "/s2container.inc.php";
-require_once dirname(dirname(__FILE__)) . "/s2dao.inc.php";
+require_once 'S2Container/S2Container.php';
+require_once dirname(dirname(__FILE__)) . '/S2Dao.php';
 
-define("PDO_DICON", dirname(__FILE__) . "/pdo.dicon");
-
+define('PDO_DICON', dirname(__FILE__) . '/pdo.dicon');
 define('S2CONTAINER_PHP5_LOG_LEVEL', S2Container_SimpleLogger::DEBUG);
 
-/** __autoload function */
-if( class_exists("S2ContainerClassLoader") ){
+if(class_exists('S2ContainerClassLoader')){
+    S2ContainerClassLoader::import(S2CONTAINER_PHP5);
     function __autoload($class = null){
-        if( S2ContainerClassLoader::load($class) ){
+        if(S2ContainerClassLoader::load($class)){
             return;
         }
     }
