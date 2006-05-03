50bb519d6ace0b6d6415745a189100c6bf201d7a
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2006-05-03 00:50:16 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@279 18cb923b-d800-0410-ad6b-a912cf186b9e

--- examples/s2dao_comment/example.inc.php
+++ examples/s2dao_comment/example.inc.php
@@ -1,15 +1,20 @@
 <?php
-require dirname(dirname(__FILE__)) . "/example.inc.php";
+require_once 'S2Container/S2Container.php';
+require_once 'S2Dao/S2Dao.php';
 
+define('S2CONTAINER_PHP5_LOG_LEVEL', S2Container_SimpleLogger::DEBUG);
+define('DAO_DICON', dirname(dirname(__FILE__)) . '/dao.dicon');
+define('PDO_DICON', dirname(dirname(__FILE__)) . '/pdo.dicon');
 define('S2DAO_PHP5_USE_COMMENT', true);
 
-if(class_exists('S2Container_AnnotationContainer')){
-    S2Container_AnnotationContainer::$DEFAULT_ANNOTATION_READER = 'S2DaoAnnotationReader';
-}
-
-if(class_exists("S2ContainerClassLoader")){
+if(class_exists('S2ContainerClassLoader')){
+    S2ContainerClassLoader::import(S2CONTAINER_PHP5);
     S2ContainerClassLoader::import(dirname(__FILE__) . "/dao");
     S2ContainerClassLoader::import(dirname(__FILE__) . "/entity");
+    function __autoload($class = null){
+        if(S2ContainerClassLoader::load($class)){
+            return;
+        }
+    }
 }
-
 ?>
