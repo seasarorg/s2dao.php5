505e619741a4692a1d499c67f150f58b18372f9c
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2006-05-18 12:33:42 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@321 18cb923b-d800-0410-ad6b-a912cf186b9e

--- examples/s2activerecord/CdClient_call.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . "/example.inc.php";
-require dirname(__FILE__) . "/classes/cd.class.php";
-
-$container = S2ContainerFactory::create("./resource/cd.dicon.xml");
-$cd = $container->getComponent("cd");
-
-var_dump($cd->find_all());
-?>
--- examples/s2activerecord/classes/cd_find_all_mysql.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM CD WHERE ID != 1;
