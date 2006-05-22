6b3e191cd91ea96f546fe590d67e88ae1c264fd9
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2006-05-22 18:58:13 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/trunk@345 18cb923b-d800-0410-ad6b-a912cf186b9e

---
+++ test/ExtensionTests.php
@@ -0,0 +1,22 @@
+<?php
+require_once dirname(__FILE__) . "/test.condition.php";
+//require_once dirname(__FILE__) . "/setup.php";
+
+S2ContainerClassLoader::import(PACKAGE_DIR . "/extension/dataset");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/extension/db");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/extension/tx");
+
+class ExtensionTests {
+    function __construct(){}
+    public static function main(){
+        PHPUnit2_TextUI_TestRunner::run(self::suite());
+    }
+    public static function suite(){
+        $suites = new PHPUnit2_Framework_TestSuite("All S2DAO_PHP5 Extensions tests");
+        return $suites;
+    }
+}
+
+ExtensionTests::main();
+
+?>
---
+++ test/S2DaoTests.php
@@ -0,0 +1,35 @@
+<?php
+require_once dirname(__FILE__) . "/test.condition.php";
+//require_once dirname(__FILE__) . "/setup.php";
+
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/annotation");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/annotation/type");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/context");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/dbms");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/id");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/impl");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/interceptors");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/pager");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/parser");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/unit");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/util");
+S2ContainerClassLoader::import(PACKAGE_DIR . "/dao/util/procedure");
+
+class S2DaoTests {
+    function __construct(){}
+    
+    public static function main(){
+        PHPUnit2_TextUI_TestRunner::run(self::suite());
+    }
+    
+    public static function suite(){
+        $suites = new PHPUnit2_Framework_TestSuite("All S2DAO_PHP5 tests");
+        $suites->addTest(AnnotationAllTest::suite());
+        return $suites;
+    }
+}
+
+S2DaoTests::main();
+echo PHP_EOL;
+?>
