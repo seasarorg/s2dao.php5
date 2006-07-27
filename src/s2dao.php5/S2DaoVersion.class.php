c4d3d6e6d433188078a35ecf264339ce9a857ba6
nowel <nowel@18cb923b-d800-0410-ad6b-a912cf186b9e>
2006-07-27 17:09:10 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/branches/S2Dao.PHP5-1.2.0@491 18cb923b-d800-0410-ad6b-a912cf186b9e

---
+++ src/s2dao.php5/S2DaoVersion.class.php
@@ -0,0 +1,55 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+// +----------------------------------------------------------------------+
+// | PHP version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright 2005-2006 the Seasar Foundation and the Others.            |
+// +----------------------------------------------------------------------+
+// | Licensed under the Apache License, Version 2.0 (the "License");      |
+// | you may not use this file except in compliance with the License.     |
+// | You may obtain a copy of the License at                              |
+// |                                                                      |
+// |     http://www.apache.org/licenses/LICENSE-2.0                       |
+// |                                                                      |
+// | Unless required by applicable law or agreed to in writing, software  |
+// | distributed under the License is distributed on an "AS IS" BASIS,    |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                        |
+// | either express or implied. See the License for the specific language |
+// | governing permissions and limitations under the License.             |
+// +----------------------------------------------------------------------+
+// | Authors: nowel                                                       |
+// +----------------------------------------------------------------------+
+// $Id$
+//
+/**
+ * @author nowel
+ */
+final class S2DaoVersion {
+    
+    const Version = '1.2.0';
+    const ReleaseVersion = 'RC';
+    const FullVersion = 'S2Dao.PHP5-1.2.0-RC';
+    
+    public static function compatibleTo($containerVersion){
+    }
+    
+    public static function compatibleS2Container(S2ContainerVersion $container){
+    }
+    
+    public static function compatiblePDO(PDO $pdo){
+    }
+    
+    public static function compatibleWith($element){
+        if($element instanceof S2ContainerVersion){
+            return self::compatibleS2Container($element);
+        }
+        if($element instanceof PDO){
+            return self::compatiblePDO($element);
+        }
+    }
+    
+    public static function requiresExtention(){
+    }
+}
+
+?>
