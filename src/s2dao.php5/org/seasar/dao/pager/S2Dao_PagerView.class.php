c576a990847ff3d30f989e2d2675452e8d433fa3
yonekawa <yonekawa@18cb923b-d800-0410-ad6b-a912cf186b9e>
2006-08-15 15:28:28 +0000

    git-svn-id: https://www.seasar.org/svn/s2dao.php5/branches/S2Dao.PHP5-1.2.0@496 18cb923b-d800-0410-ad6b-a912cf186b9e

---
+++ src/s2dao.php5/org/seasar/dao/pager/S2Dao_PagerView.class.php
@@ -0,0 +1,167 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+// +----------------------------------------------------------------------+
+// | PHP version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright 2005-2006 the Seasar Foundation and the Others.            |
+// +----------------------------------------------------------------------+
+// | Licensed under the Apache License, Version 2.0 (the "License");      |
+// | you may not use this file except in compliance with the License.     |
+// | You may obtain a copy of the License at                              |
+// |                                                                      |
+// |     http://www.apache.org/licenses/LICENSE-2.0                       |
+// |                                                                      |
+// | Unless required by applicable law or agreed to in writing, software  |
+// | distributed under the License is distributed on an "AS IS" BASIS,    |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                        |
+// | either express or implied. See the License for the specific language |
+// | governing permissions and limitations under the License.             |
+// +----------------------------------------------------------------------+
+// | Authors: yonekawa                                                    |
+// +----------------------------------------------------------------------+
+// $Id$
+//
+/**
+ * @author yonekawa
+ */
+class S2Dao_PagerView
+{
+    private $helper = null;
+
+    private previewLink = '';
+    private nextLink = '';
+    private pageLinks = '';
+
+    public function __construct($dto)
+    {
+        $this->helper = new S2Dao_PagerViewHelper($dto);
+        if ($this->helper->isPrev()) {
+            $this->makePreviewLink();
+        }
+        if ($this->helper->isNext()) {
+            $this->makeNextLink();
+        }
+    }
+
+    public function makePreviewLink($dto) 
+    {
+    }
+    <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+// +----------------------------------------------------------------------+
+// | PHP version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright 2005-2006 the Seasar Foundation and the Others.            |
+// +----------------------------------------------------------------------+
+// | Licensed under the Apache License, Version 2.0 (the "License");      |
+// | you may not use this file except in compliance with the License.     |
+// | You may obtain a copy of the License at                              |
+// |                                                                      |
+// |     http://www.apache.org/licenses/LICENSE-2.0                       |
+// |                                                                      |
+// | Unless required by applicable law or agreed to in writing, software  |
+// | distributed under the License is distributed on an "AS IS" BASIS,    |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                        |
+// | either express or implied. See the License for the specific language |
+// | governing permissions and limitations under the License.             |
+// +----------------------------------------------------------------------+
+// | Authors: yonekawa                                                    |
+// +----------------------------------------------------------------------+
+// $Id$
+//
+/**
+ * @author yonekawa
+ */
+class S2Dao_PagerView
+{
+    private $helper = null;
+    
+    private $href = '';
+    private $previewLink = '';
+    private $nextLink = '';
+    private $pageLinks = '';
+
+    public function __construct(S2Dao_PagerCondition $dto, $href = '')
+    {
+        $this->href = $href;
+
+        $this->helper = new S2Dao_PagerViewHelper($dto);
+        if ($this->helper->isPrev()) {
+            $this->makePreviewLink($dto->getLimit());
+        } else {
+            $this->makeNonePreviewLink();
+        }
+        if ($this->helper->isNext()) {
+            $this->makeNextLink($dto->getLimit());
+        } else {
+            $this->makeNoneNextLink();
+        }
+        
+        $this->makePageLinks();
+    }
+
+    /**
+     * 前の○件リンクを作成する
+     */
+    public function makePreviewLink($limit) 
+    {
+        if ($limit <= 0) {
+            return;
+        }
+        if (!$this->helper->isPrev()) {
+        }
+        $this->previewLink = '<a href="' . $href . '">前の' . $limit . '件</a>';
+    }
+    
+    /**
+     * 次の○件リンクを作成する
+     */
+    public function makeNextLink($limit)
+    {
+        if ($limit <= 0) {
+            return;
+        }
+        if (!$this->helper->isNext()) {
+            $this->makeNoneNextLink($limit);
+            return;
+        }
+        $this->nextLink = '<a href="' . $href . '">次の' . $limit . '件</a>';
+    }
+    
+    /**
+     * ページ番号リンクを作成する
+     */
+    public function makePageLinks()
+    {
+        $begin = $this->helper->getDisplayPageIndexBegin();
+        $end = $this->helper->getDisplayPageIndexEnd();
+        $index = $this->helper->getPageIndex();
+
+        for ( $i = $begin; $i <= $end; $i++ ) {
+            if ($i == $index) {
+                $this->pageLinks .= ($i + 1) . ' ';
+            } else {
+                $this->pageLinks .= '<a href="' . $href . '">' . ( $i + 1 ) . '</a> ';
+            }
+        }
+    }
+
+    /**
+     * 次の○件が存在しない時の出力を作成する
+     */
+    public function makeNoneNextLink($limit)
+    {
+        $this->nextLink = '次の' . $limit . '件';
+    }
+    
+    /**
+     * 前の○件が存在しない時の出力を作成する
+     */
+    public function makeNonePreviewLink($limit)
+    {
+        $this->previewLink = '前の' . $limit . '件';
+    }
+    
+}
+
+?>
